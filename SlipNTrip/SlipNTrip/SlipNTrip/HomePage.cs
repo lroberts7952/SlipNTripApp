dab9d918c571e08b3d7f1833bcac13161700129d
lroberts7952 <61805480+lroberts7952@users.noreply.github.com>
2021-09-25 00:11:51 -0400

    Add files via upload

---
+++ SlipNTrip/SlipNTrip/SlipNTrip/HomePage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace SlipNTrip
+{
+    public class HomePage : ContentPage
+    {
+        private Button addPatient;
+        private Button existingPatients;
+        private Button deviceControls;
+
+        public HomePage()
+        {
+            StackLayout stackLayout = new StackLayout();
+            this.Title = "Slip N Trip App";
+
+            addPatient = new Button();
+            addPatient.Text = "Add Patient";
+            addPatient.Clicked += OnAddPatientButtonClicked;
+            stackLayout.Children.Add(addPatient);
+
+            existingPatients = new Button();
+            existingPatients.Text = "Existing Patient";
+            existingPatients.Clicked += OnExistingPatientsButtonClicked;
+            stackLayout.Children.Add(existingPatients);
+
+            deviceControls = new Button();
+            deviceControls.Text = "Device Controls";
+            //deviceControls.Clicked += OnDeviceControlsButtonClicked;
+            stackLayout.Children.Add(deviceControls);
+
+            Content = stackLayout;
+
+        }
+
+        async void OnAddPatientButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddPatient());
+        }
+
+        async void OnExistingPatientsButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ExistingPatient());
+        }
+    }
+}

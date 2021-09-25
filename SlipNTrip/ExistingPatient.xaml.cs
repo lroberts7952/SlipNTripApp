532dedeff491044992bc8aafc602375f97bc4810
lroberts7952 <61805480+lroberts7952@users.noreply.github.com>
2021-09-25 00:16:08 -0400

    Add files via upload

---
+++ SlipNTrip/ExistingPatient.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SlipNTrip
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ExistingPatient : ContentPage
+    {
+        public ExistingPatient()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            collectionView.ItemsSource = await App.PatientDatabase.GetPeopleAsync();
+        }
+    }
+}
---
+++ SlipNTrip/ExistingPatients.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace SlipNTrip
+{
+    public class ExistingPatients : ContentPage
+    {
+        private ListView listView;
+        public ExistingPatients()
+        {
+            this.Title = "Patients";
+
+            StackLayout stackLayout = new StackLayout();
+
+            listView = new ListView();
+            //listView.ItemsSource = App.PatientDatabase.GetPeopleAsync();
+        }
+    }
+}

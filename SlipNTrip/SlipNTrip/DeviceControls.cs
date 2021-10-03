7a8aed3be2ab6d6bb4ac0306929078039d6f86ae
lroberts7952 <61805480+lroberts7952@users.noreply.github.com>
2021-10-03 14:31:50 -0400

    Add files via upload

---
+++ SlipNTrip/SlipNTrip/DeviceControls.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace SlipNTrip
+{
+    public class DeviceControls : ContentPage
+    {
+        private Button returnHome;
+        public DeviceControls()
+        {
+            this.Title = "Device Controls";
+
+            StackLayout stackLayout = new StackLayout();
+
+            returnHome = new Button();
+            returnHome.Text = "Return to Home Page";
+            returnHome.Clicked += returnHomeButtonClicked;
+            stackLayout.Children.Add(returnHome);
+
+            Content = stackLayout;
+        }
+
+        async void returnHomeButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopToRootAsync();
+        }
+    }
+}
---
+++ SlipNTrip/SlipNTrip/ExistingPatients.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using SQLite;
+
+using Xamarin.Forms;
+
+namespace SlipNTrip
+{
+    public class ExistingPatients : ContentPage
+    {
+        private ListView listView;
+        string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "patients.db3");
+
+        Patient patient = new Patient();
+
+        public ExistingPatients()
+        {
+            this.Title = "Patients";
+            var db = new SQLiteConnection(dbPath);
+
+            StackLayout stackLayout = new StackLayout();
+
+            SearchBar searchBar = new SearchBar { Placeholder = "Search items..." };
+            searchBar.TextChanged += OnTextChanged;
+            stackLayout.Children.Add(searchBar);
+
+            listView = new ListView();
+            listView.ItemsSource = db.Table<Patient>().OrderBy(x => x.Name).ToList();
+            listView.ItemSelected += listView_ItemSelected;
+            listView.BackgroundColor = Color.White;
+
+            stackLayout.Children.Add(listView);
+            Content = new ScrollView { Content = stackLayout };
+        }
+
+        private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            patient = (Patient)e.SelectedItem;
+            Navigation.PushAsync(new PatientProfile(patient));
+        }
+
+        private void OnTextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
